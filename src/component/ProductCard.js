d8dbf36081729c570d3cdcb1b388283e1c8055b2
pistapixie <hleehslee@gmail.com>
2024-03-28 01:51:45 +0900

    [Feat] 동적으로 리스트 랜더링 구현

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  const { img, title, price, choice, new: isNew, size } = product;
+
   return (
     <div>
-      <img
-        width={250}
-        src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fa3%2Ff1%2Fa3f12b80a167edce999c7fd53bb4c0a3a7b1ddd5.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]"
-        alt=""
-      />
-      <div>Conscious choice</div>
-      <div>벨티드 트윌 코트</div>
-      <div>₩99900</div>
-      <div>신제품</div>
+      <img width={250} src={img} alt={title} />
+      <div>{title}</div>
+      <div>₩{price}</div>
+      {choice && <div>Conscious choice</div>}
+      {isNew && <div>신제품</div>}
+      <div>사이즈: {size.join(", ")}</div>
     </div>
   );
 };
