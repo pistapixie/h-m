089dd9d16bfe6c01d0bfd28638f635d697cd9341
pistapixie <hleehslee@gmail.com>
2024-03-28 01:29:38 +0900

    [Feat]api 호출 구현

---
+++ src/component/ProductCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img
+        width={250}
+        src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fa3%2Ff1%2Fa3f12b80a167edce999c7fd53bb4c0a3a7b1ddd5.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]"
+        alt=""
+      />
+      <div>Conscious choice</div>
+      <div>벨티드 트윌 코트</div>
+      <div>₩99900</div>
+      <div>신제품</div>
+    </div>
+  );
+};
+
+export default ProductCard;
