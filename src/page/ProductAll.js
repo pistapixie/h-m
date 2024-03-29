fbcf6932637e439b0339fe07f4ab029b531ce196
pistapixie <hleehslee@gmail.com>
2024-03-29 19:18:24 +0900

    [Modify]db.json url 변경

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -11,7 +11,7 @@ const ProductAll = () => {
   useEffect(() => {
     const getProducts = async () => {
       let searchQuery = query.get("q") || "";
-      let url = `http://localhost:5000/products?q=${searchQuery}`;
+      let url = `https://my-json-server.typicode.com/pistapixie/h-m/products?q=${searchQuery}`;
       try {
         let response = await fetch(url);
         let data = await response.json();
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -8,7 +8,7 @@ const ProductDetail = () => {
 
   useEffect(() => {
     const getProductDetail = async () => {
-      let url = `http://localhost:5000/products/${id}`;
+      let url = `https://my-json-server.typicode.com/pistapixie/h-m/products/${id}`;
       let response = await fetch(url);
       let data = await response.json();
       setProduct(data);
