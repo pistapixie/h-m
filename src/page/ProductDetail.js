0aad1aad9b2cc4bb49017c20d9abbe5c2dae7585
pistapixie <hleehslee@gmail.com>
2024-03-31 23:51:52 +0900

    [Modify] 주석 삭제

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -18,32 +18,34 @@ const ProductDetail = () => {
   }, [id]);
 
   return (
-    <Container className="product-detail-container">
-      <Row>
-        <Col className="product-img">
-          <img src={product?.img} alt={product?.title} />
-        </Col>
-        <Col>
-          <h2>{product?.title}</h2>
-          <p>가격: ₩{product?.price}</p>
-          {product?.choice === true && <p>Conscious choice</p>}
-          {product?.new === true && <p>신제품</p>}
-          <div>
-            <label htmlFor="size-select">사이즈:</label>
-            <select id="size-select" className="custom-select">
-              {product?.size?.map((size, index) => (
-                <option key={index} value={size}>
-                  {size}
-                </option>
-              ))}
-            </select>
-          </div>
-          <Button className="add-button" variant="danger">
-            추가
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+    <div className="product-detail-container">
+      <Container>
+        <Row>
+          <Col className="product-img">
+            <img src={product?.img} alt={product?.title} />
+          </Col>
+          <Col>
+            <h2>{product?.title}</h2>
+            <p>가격: ₩{product?.price}</p>
+            {product?.choice === true && <p>Conscious choice</p>}
+            {product?.new === true && <p>신제품</p>}
+            <div>
+              <label htmlFor="size-select">사이즈:</label>
+              <select id="size-select" className="custom-select">
+                {product?.size?.map((size, index) => (
+                  <option key={index} value={size}>
+                    {size}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <Button className="add-button" variant="danger">
+              추가
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
